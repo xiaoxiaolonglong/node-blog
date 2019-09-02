8fc471e98ab8acf9a43f995a1ef509f86988ff3a
lzh <1096495142@qq.com.com>
2019-09-02 19:24:17 +0800

    绑定接口

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/base.css'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,32 +1,48 @@
 <template>
-    <el-container>
-        <div class="content-box">
-          <h2>标题</h2>
+    <div>
+        <div v-for="(data,index) in blogList" :key="index" class="content-box">
+          <h2>{{data.title}}</h2>
           <div class="clearfix">
             <div class="left-image-box">
               <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
             </div>
             <div class="right-content-box">
               <span>
-                高阶线性差分方程： 很多答案侧重于不干涉他人自由，讨论『奥特曼』这部作品为什么值得一看的回答不太多。 
-                我就写一下这篇回答，内容有限，，只能讲述一点点奥特曼的精髓，权当抛砖引玉了。 言归正传 “没有…
+                {{data.content}}
               </span>
-              <el-button type="text" class="button"><router-link :to="{ path:'detail',query:{id:'1110'}}">详情</router-link></el-button>
+              <el-button type="text" class="button"><router-link :to="{ path:'detail',query:{id:data.id}}">详情</router-link></el-button>
             </div>
           </div>
           <div class="operate-btn">
             <el-button type="primary" icon="el-icon-caret-top">赞同</el-button>
           </div>
         </div>
-    </el-container>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            currentDate: new Date()
+            currentDate: new Date(),
+            blogList:[]
         };
+    },
+    mounted(){
+        //在页面加载时就触发请求数据的函数
+        //用self存储当前组件vue对象的this
+        let self = this;
+        //使用axios请求数据
+        this.axios.get("/api/blog/list")
+            .then(function (response) {
+                //此处要使用self，如果使用this指向的是axios对象，不再是vue对象
+                // self.movieList = response.data;
+                self.blogList = response.data.data
+                console.log(self.blogList)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 }
 </script>
